ceb19a42f0960154ca83beb1bf0dafdf83ff4542
Ryan Ghaida <ryaboug@gmail.com>
2018-04-09 01:38:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
-# your code goes here
+def begins_with_r (array)
+
+  arr = []
+
+  array.each do |words|
+    if words.start_with?('r')
+     arr << true
+    else
+     arr <<  false
+    end
+
+  puts arr
+
+  end
+
+  arr.include?("false")
+
+  end
+
+  def contain_a (array)
+    arr = [] 
+  array.each do |words| 
+    if words.start_with?("a")
+      arr << words
+    end
+return arr 
+end 
+  
